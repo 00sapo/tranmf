185b4db33ba769fa57c057813b7490c2432f2e5e
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-02-13 15:51:32 +0100

    Moved from generics to type alias

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 use genevo::{genetic::Parents, operator::prelude::*, operator::CrossoverOp, prelude::*};
 use memory_stats::memory_stats;
 use ndarray::prelude::*;
-use num::{abs, FromPrimitive, Integer, Signed, ToPrimitive};
+use ndarray_stats::QuantileExt;
 use std::fmt::Debug;
 use std::ops::Sub;
 
@@ -32,41 +32,25 @@ struct Parameter {
 impl Default for Parameter {
     fn default() -> Self {
         Self {
-            population_size: 24,
-            generation_limit: 50,
-            num_individuals_per_parents: 10,
+            population_size: 4000,
+            generation_limit: 50000,
+            num_individuals_per_parents: 200,
             selection_ratio: 0.5,
             // num_crossover_points: 2,
-            mutation_rate: 0.02, //25,
+            mutation_rate: 0.01, //25,
             reinsertion_ratio: 0.5,
         }
     }
 }
 
-trait MyNum:
-    Integer + FromPrimitive + ToPrimitive + Signed + Debug + Clone + Send + Sync + PartialEq
-{
-}
+type Int = i16;
 
-impl MyNum for i8 {}
-
-impl MyNum for i16 {}
-
-impl MyNum for i32 {}
-
-impl MyNum for i64 {}
-
-impl MyNum for i128 {}
-
-fn reconstruct_3d_array<T>(arr: &Array2<T>, shape2: usize) -> Array3<i8>
-where
-    T: MyNum,
-{
+fn reconstruct_3d_array(arr: &Array2<Int>, shape2: usize) -> Array3<i8> {
     // construct the 3d array
     let mut arr3d = Array3::<i8>::default((arr.shape()[0], arr.shape()[1], shape2));
     for i in 0..arr.shape()[0] {
         for j in 0..arr.shape()[1] {
-            let k = arr[[i, j]].to_usize().unwrap();
+            let k = arr[[i, j]] as usize;
             if k < shape2 {
                 arr3d[[i, j, k]] = 1;
             }
@@ -77,23 +61,17 @@ where
 
 /// The genotype
 #[derive(PartialEq, Debug, Clone)]
-struct Genome<T>
-where
-    T: MyNum,
-{
-    arr: Array2<T>,
-    projections: (Array2<T>, Array2<T>, Array2<T>),
+struct Genome {
+    arr: Array2<Int>,
+    projections: (Array2<Int>, Array2<Int>, Array2<Int>),
 }
 
-impl<T> Genome<T>
-where
-    T: MyNum,
-{
-    fn from_arr(arr: Array2<T>, shape2: usize) -> Self {
+impl Genome {
+    fn from_arr(arr: Array2<Int>, shape2: usize) -> Self {
         let arr3d = reconstruct_3d_array(&arr, shape2);
-        let proj_0 = arr3d.sum_axis(Axis(0)).mapv(|x| T::from_i8(x).unwrap());
-        let proj_1 = arr3d.sum_axis(Axis(1)).mapv(|x| T::from_i8(x).unwrap());
-        let proj_2 = arr3d.sum_axis(Axis(2)).mapv(|x| T::from_i8(x).unwrap());
+        let proj_0 = arr3d.sum_axis(Axis(0)).mapv(|x| x as Int);
+        let proj_1 = arr3d.sum_axis(Axis(1)).mapv(|x| x as Int);
+        let proj_2 = arr3d.sum_axis(Axis(2)).mapv(|x| x as Int);
         Genome {
             arr,
             projections: (proj_0, proj_1, proj_2),
@@ -104,13 +82,13 @@ where
     /// Then computes the arg-where of the ones and stores it into a genome.
     fn from_shape(
         shape: (usize, usize, usize),
-        shape2_mask: &Array2<T>,
+        shape2_mask: &Array2<Int>,
         rng: &mut impl Rng,
     ) -> Self {
         let mut arr3d = Array3::<bool>::default(shape);
         for i in 0..shape.0 {
             for j in 0..shape.1 {
-                if shape2_mask[[i, j]] != T::zero() {
+                if shape2_mask[[i, j]] != Int::zero() {
                     let k = rng.gen_range(0..shape.2);
                     arr3d[[i, j, k]] = true;
                 }
@@ -118,24 +96,21 @@ where
         }
 
         // convert the index of the 1s along the 2nd axis to their indices
-        let arr = Array2::<T>::from_shape_fn((shape.0, shape.1), |(i, j)| {
-            T::from_usize(
-                (0..shape.2)
-                    .position(|k| arr3d[[i, j, k]])
-                    .unwrap_or(shape.2),
-            )
-            .unwrap()
+        let arr = Array2::<Int>::from_shape_fn((shape.0, shape.1), |(i, j)| {
+            (0..shape.2)
+                .position(|k| arr3d[[i, j, k]])
+                .unwrap_or(shape.2) as Int
         });
 
         // compute the projections
         let proj_0 = arr3d
-            .mapv(|x| if x { T::one() } else { T::zero() })
+            .mapv(|x| if x { 1 as Int } else { Int::zero() })
             .sum_axis(Axis(0));
         let proj_1 = arr3d
-            .mapv(|x| if x { T::one() } else { T::zero() })
+            .mapv(|x| if x { 1 as Int } else { Int::zero() })
             .sum_axis(Axis(1));
         let proj_2 = arr3d
-            .mapv(|x| if x { T::one() } else { T::zero() })
+            .mapv(|x| if x { 1 as Int } else { Int::zero() })
             .sum_axis(Axis(2));
 
         Genome {
@@ -144,7 +119,7 @@ where
         }
     }
 
-    pub fn project(&self, axis: usize) -> &Array2<T> {
+    pub fn project(&self, axis: usize) -> &Array2<Int> {
         // convert the bool array to an array of 1 and 0 of type i32
         match axis {
             0 => &self.projections.0,
@@ -155,34 +130,25 @@ where
     }
 }
 
-impl<T> Genotype for Genome<T>
-where
-    T: MyNum,
-{
-    type Dna = T;
+impl Genotype for Genome {
+    type Dna = Int;
 }
 
 /// The fitness function for `Genome`s.
 #[derive(Clone, Debug)]
-struct FitnessCalc<T>
-where
-    T: MyNum,
-{
-    target_projections: (Array2<T>, Array2<T>, Array2<T>),
-    max_diff: T,
+struct FitnessCalc {
+    target_projections: (Array2<Int>, Array2<Int>, Array2<Int>),
+    max_diff: Int,
 }
 
-impl<T> FitnessCalc<T>
-where
-    T: MyNum,
-{
-    pub fn new(target_projections: (Array2<T>, Array2<T>, Array2<T>)) -> Self {
+impl FitnessCalc {
+    pub fn new(target_projections: (Array2<Int>, Array2<Int>, Array2<Int>)) -> Self {
         let genome_shape = (
             target_projections.1.shape()[0],
             target_projections.0.shape()[0],
             target_projections.0.shape()[1],
         );
-        let max_diff = T::from_usize(genome_shape.0 * genome_shape.1 * genome_shape.2 * 2).unwrap();
+        let max_diff = (genome_shape.0 * genome_shape.1 * genome_shape.2 * 2) as Int;
         FitnessCalc {
             target_projections,
             max_diff,
@@ -190,11 +156,8 @@ where
     }
 }
 
-impl<T> FitnessFunction<Genome<T>, usize> for FitnessCalc<T>
-where
-    T: MyNum,
-{
-    fn fitness_of(&self, genome: &Genome<T>) -> usize {
+impl FitnessFunction<Genome, usize> for FitnessCalc {
+    fn fitness_of(&self, genome: &Genome) -> usize {
         // compute the projections of the genome
         let proj_0 = genome.project(0);
         let proj_1 = genome.project(1);
@@ -202,13 +165,17 @@ where
 
         // take the sum of the absolute differences
         // between the projections and the target projections
-        let diff_0 = (proj_0 - &self.target_projections.0).mapv(|x| abs(x)).sum();
-        let diff_1 = (proj_1 - &self.target_projections.1).mapv(|x| abs(x)).sum();
+        let diff_0 = (proj_0 - &self.target_projections.0)
+            .mapv(|x| x.abs())
+            .sum();
+        let diff_1 = (proj_1 - &self.target_projections.1)
+            .mapv(|x| x.abs())
+            .sum();
         // let diff_2 = (proj_2 - &self.target_projections.2).mapv(|x| abs(x)).sum();
         let diff = diff_0 + diff_1; // + diff_2;
 
         // subtract from the maximum
-        (self.max_diff.clone() - diff).to_usize().unwrap()
+        (self.max_diff - diff) as usize
     }
 
     fn average(&self, fitness_values: &[usize]) -> usize {
@@ -216,7 +183,7 @@ where
     }
 
     fn highest_possible_fitness(&self) -> usize {
-        self.max_diff.to_usize().unwrap()
+        self.max_diff as usize
     }
 
     fn lowest_possible_fitness(&self) -> usize {
@@ -224,11 +191,8 @@ where
     }
 }
 
-impl<T> CrossoverOp<Genome<T>> for UniformCrossBreeder
-where
-    T: MyNum,
-{
-    fn crossover<R>(&self, parents: Parents<Genome<T>>, rng: &mut R) -> Vec<Genome<T>>
+impl CrossoverOp<Genome> for UniformCrossBreeder {
+    fn crossover<R>(&self, parents: Parents<Genome>, rng: &mut R) -> Vec<Genome>
     where
         R: Rng + Sized,
     {
@@ -236,7 +200,7 @@ where
         let genome_shape_2 = parents[0].projections.0.shape()[1];
         let num_parents = parents.len();
         // breed one child for each partner in parents
-        let mut children: Vec<Genome<T>> = Vec::with_capacity(num_parents);
+        let mut children: Vec<Genome> = Vec::with_capacity(num_parents);
         while num_parents > children.len() {
             let mut genome_arr = parents[0].arr.clone();
             for sp in 0..num_parents {
@@ -246,10 +210,11 @@ where
                     let i = rng.gen_range(0..genome_shape[0]);
                     let j = rng.gen_range(0..genome_shape[1]);
                     // assign the average value of the two parents
-                    // // TODO: differential evolution here
-                    // // respecting shape2 projection
-                    // let avg: T = (parents[0].arr[[i, j]].clone() + parents[sp].arr[[i, j]].clone())
-                    //     / T::from_usize(2).unwrap();
+                    // TODO: differential evolution here
+                    // respecting shape2 projection
+                    // let avg: Int = (parents[0].arr[[i, j]].clone() - parents[sp].arr[[i, j]].clone())
+                    //     * T::from_f64(0.5).unwrap()
+                    //     + parents[0].arr[[i, j]].clone();
                     // genome_arr[[i, j]] = avg;
                     // copy the value from the 2nd parent
                     genome_arr
@@ -263,11 +228,8 @@ where
     }
 }
 
-impl<T> RandomGenomeMutation for Genome<T>
-where
-    T: MyNum,
-{
-    type Dna = T;
+impl RandomGenomeMutation for Genome {
+    type Dna = Int;
 
     fn mutate_genome<R>(
         genome: Self,
@@ -280,14 +242,12 @@ where
         R: Rng + Sized,
     {
         // randomly mutate the genome
-        let min = (*min_value).to_usize().unwrap();
-        let max = (*max_value).to_usize().unwrap();
 
         let mut new_genome = genome.clone();
         for i in 0..genome.arr.shape()[0] {
             for j in 0..genome.arr.shape()[1] {
-                if rng.gen_bool(mutation_rate) && (genome.projections.2[[i, j]] != T::zero()) {
-                    new_genome.arr[[i, j]] = T::from_usize(rng.gen_range(min..max)).unwrap();
+                if rng.gen_bool(mutation_rate) && (genome.projections.2[[i, j]] != Int::zero()) {
+                    new_genome.arr[[i, j]] = rng.gen_range(*min_value..*max_value);
                 }
             }
         }
@@ -295,28 +255,22 @@ where
     }
 }
 
-impl<T> Sub for Genome<T>
-where
-    T: MyNum,
-{
-    type Output = Array3<f32>;
+impl Sub for Genome {
+    type Output = Array3<i8>;
     fn sub(self, other: Self) -> Self::Output {
         // reconstruct the 3d array
         let arr3d_1 = reconstruct_3d_array(&self.arr, self.projections.2.shape()[1]);
         let arr3d_2 = reconstruct_3d_array(&other.arr, other.projections.2.shape()[1]);
-        (arr3d_1 - arr3d_2).mapv(|x| abs(x) as f32)
+        (arr3d_1 - arr3d_2).mapv(|x| x.abs())
     }
 }
 
-fn new_population<T>(
+fn new_population(
     size: usize,
     shape: (usize, usize, usize),
-    shape2_mask: &Array2<T>,
+    shape2_mask: &Array2<Int>,
     rng: &mut impl Rng,
-) -> Population<Genome<T>>
-where
-    T: MyNum,
-{
+) -> Population<Genome> {
     // generate a vec of genomes with p distributed from 0 to 1 with step 1 / size
     let mut pop = Vec::with_capacity(size);
     for _ in 0..size {
@@ -328,12 +282,12 @@ where
 // Exported function for Python
 #[no_mangle]
 pub extern "C" fn run_genetic_algorithm() {
-    let shape = (1000, 1000, 100);
+    let shape = (10, 10, 10);
     let rng = &mut rand::thread_rng();
     let p = 0.5;
     // generate a random boolean matrix as a mask for the 3rd axis
     let shape2_mask =
-        Array2::<i32>::from_shape_fn(
+        Array2::<Int>::from_shape_fn(
             (shape.0, shape.1),
             |(_, _)| {
                 if rng.gen_bool(p) {
@@ -344,7 +298,7 @@ pub extern "C" fn run_genetic_algorithm() {
             },
         );
 
-    let ground_truth = Genome::<i32>::from_shape(shape, &shape2_mask, rng);
+    let ground_truth = Genome::from_shape(shape, &shape2_mask, rng);
     let fitness_calc = FitnessCalc::new(ground_truth.projections.clone());
 
     let params = Parameter::default();
@@ -366,8 +320,8 @@ pub extern "C" fn run_genetic_algorithm() {
             .with_crossover(UniformCrossBreeder::default())
             .with_mutation(RandomValueMutator::new(
                 params.mutation_rate,
-                0i32,
-                shape.2 as i32,
+                0 as Int,
+                shape.2 as Int,
             ))
             .with_reinsertion(ElitistReinserter::new(
                 fitness_calc.clone(),
@@ -392,28 +346,22 @@ pub extern "C" fn run_genetic_algorithm() {
             Ok(SimResult::Intermediate(step)) => {
                 let evaluated_population = step.result.evaluated_population;
                 let best_solution = step.result.best_solution;
-                let best_proj2 = best_solution.solution.genome.project(2);
                 let diff = (ground_truth.clone() - best_solution.solution.genome.clone()).sum()
                     as f32
-                    / (shape.0 * shape.1 * shape.2) as f32;
-                let diff_proj_2 = (best_proj2 - &fitness_calc.target_projections.2)
-                    .mapv(|x| x.abs())
-                    .sum() as f32
-                    / ((shape.0 * shape.1) as f32);
+                    / (shape.0 * shape.1) as f32;
                 println!(
                     "Step: generation: {}, average_fitness: {}, \
-                    best fitness: {}, real diff: {}, diff proj 2: {}",
+                    best fitness: {}, real diff: {}",
                     step.iteration,
                     evaluated_population.average_fitness(),
                     best_solution.solution.fitness as f32 / fitness_calc.max_diff as f32,
                     diff,
-                    diff_proj_2
                 );
                 mem_stats();
             }
             Ok(SimResult::Final(step, _processing_time, duration, stop_reason)) => {
                 let best_solution = step.result.best_solution;
-                let diff = (ground_truth.clone() - best_solution.solution.genome.clone()).sum();
+                let diff = (ground_truth - best_solution.solution.genome).sum() as i32;
                 println!("{}", stop_reason);
                 println!(
                     "Final result after {}: generation: {}, \
