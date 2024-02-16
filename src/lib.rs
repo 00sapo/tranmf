bf73c79946fb17de19366c1ab5914b2723bef59d
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-02-16 20:00:44 +0100

    Added skeleton for nmf

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,111 @@
-use ndarray::{Array2, ArrayBase, Dimension, Ix2, RawData};
+use ndarray::{Array, ArrayBase, Data, DataOwned, Dimension, Ix2, OwnedRepr, RawData};
+use num::Zero;
 use pyo3::prelude::*;
+use std::ops::{AddAssign, MulAssign};
 
-type Float = f64;
-
-struct Loss<D: Dimension, P> {
-    Precision: Float,
-    components: Vec<Box<dyn LossComponent<D, Precision = P>>>,
+struct Loss<R: RawData, D: Dimension> {
+    components: Vec<Box<dyn LossComponent<R, D>>>,
 }
 
-impl Loss<Ix2, f64> {
-    fn compute(&self, v: &Array2<f64>) -> f64 {
-        self.components.iter().map(|c| c.compute(v)).sum()
+impl<R, D> Loss<R, D>
+where
+    R: Data,
+    D: Dimension,
+    R::Elem: Zero + Clone,
+    ArrayBase<R, D>: AddAssign,
+{
+    fn compute(&self, v: &[ArrayBase<R, D>]) -> ArrayBase<R, D> {
+        let shape = v.get(0).unwrap().raw_dim();
+        let mut computed = self.components.iter().map(|c| c.compute(v));
+        let mut sum = computed.next().unwrap();
+        computed.for_each(|c| sum += c);
+        sum
     }
 
-    fn gradient(&self, v: &Array2<f64>) -> Array2<f64> {
-        self.components
-            .iter()
-            .map(|c| c.gradient(v))
-            .fold(Array2::zeros(v.dim()), |acc, x| acc + x)
+    fn gradient(&self, v: &[ArrayBase<R, D>]) -> ArrayBase<R, D> {
+        let shape = v.get(0).unwrap().raw_dim();
+        let mut computed = self.components.iter().map(|c| c.gradient(v));
+        let mut sum = computed.next().unwrap();
+        computed.for_each(|c| sum += c);
+        sum
     }
 }
 
-trait LossComponent<D: Dimension>
-where
-    Self::Precision: RawData,
-{
-    type Precision;
-    fn compute(&self, v: Vec<ArrayBase<Self::Precision, D>>) -> Self::Precision;
-    fn gradient(&self, v: Vec<ArrayBase<Self::Precision, D>>) -> Self::Precision;
+trait LossComponent<R: RawData, D: Dimension> {
+    fn compute(&self, v: &[ArrayBase<R, D>]) -> ArrayBase<R, D>;
+    fn gradient(&self, v: &[ArrayBase<R, D>]) -> ArrayBase<R, D>;
 }
 
-trait NMF<D: Dimension>
+trait UpdateComponent<R: RawData, D: Dimension> {
+    fn update_h(
+        &self,
+        w: &ArrayBase<R, D>,
+        h: &ArrayBase<R, D>,
+        gradient: &ArrayBase<R, D>,
+    ) -> ArrayBase<R, D>;
+
+    fn update_w(
+        &self,
+        w: &ArrayBase<R, D>,
+        h: &ArrayBase<R, D>,
+        gradient: &ArrayBase<R, D>,
+    ) -> ArrayBase<R, D>;
+}
+
+enum UpdateType {
+    Multiplicative,
+    Additive,
+}
+
+struct Updater<R: RawData, D: Dimension> {
+    update_type: UpdateType,
+    components: Vec<Box<dyn UpdateComponent<R, D>>>,
+}
+
+impl<R, D> Updater<R, D>
 where
-    Self::Precision: RawData,
+    R: Data,
+    D: Dimension,
+    R::Elem: Zero + Clone,
+    ArrayBase<R, D>: MulAssign + AddAssign,
 {
-    type Precision;
-    fn run(&self, max_iter: usize);
-    fn random_initialize(&mut self);
-    fn set_w(&mut self, w: &ArrayBase<Self::Precision, D>);
-    fn get_w(&self) -> &ArrayBase<Self::Precision, D>;
-    fn set_h(&mut self, h: ArrayBase<Self::Precision, D>);
-    fn get_h(&self) -> &ArrayBase<Self::Precision, D>;
-    fn set_loss_fn(&mut self, loss_fn: Loss<D, Self::Precision>);
-    fn get_loss_fn(&self) -> Loss<D, Self::Precision>;
+    fn update(&self, w: &mut ArrayBase<R, D>, h: &mut ArrayBase<R, D>, gradient: &ArrayBase<R, D>) {
+        for component in &self.components {
+            let update_w = component.update_w(w, h, gradient);
+            let update_h = component.update_h(w, h, gradient);
+            match self.update_type {
+                UpdateType::Multiplicative => {
+                    *w *= update_w;
+                    *h *= update_h;
+                }
+                UpdateType::Additive => {
+                    *w += update_w;
+                    *h += update_h;
+                }
+            }
+        }
+    }
+}
+
+struct NmfBase<R: RawData, D: Dimension> {
+    w: ArrayBase<R, D>,
+    h: ArrayBase<R, D>,
+    loss: Loss<R, D>,
+    updater: Updater<R, D>,
+}
+
+impl<R: RawData, D: Dimension> NmfBase<R, D> {
+    fn random_initialize(&mut self) {}
+    fn add_update_component(&mut self, component: Box<dyn UpdateComponent<R, D>>) {
+        self.updater.components.push(component);
+    }
+    fn add_loss_component(&mut self, component: Box<dyn LossComponent<R, D>>) {
+        self.loss.components.push(component);
+    }
+}
+
+trait NmfComputer<R: RawData, D: Dimension> {
+    fn compute(&self, nmf: NmfBase<R, D>) -> ArrayBase<R, D>;
 }
 
 // TODO: modify
